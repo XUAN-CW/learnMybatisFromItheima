86a8c02afa95c7ffbaf6ee4139552bbc7323314f
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-10-07 21:06:31 +0800

    完成了修改操作、删除操作

--- code/No_001_itheima_mybatis_quick/src/test/java/MyBatisTest.java
+++ code/No_001_itheima_mybatis_quick/src/test/java/MyBatisTest.java
@@ -63,4 +63,50 @@ public class MyBatisTest {
         sqlSession.close();
     }
 
+    @Test
+    //修改操作
+    public void test3() throws IOException {
+
+        //模拟user对象
+        User user = new User();
+        user.setId(147);
+        user.setUsername("lucy");
+        user.setPassword("123");
+
+        //获得核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        //获得session工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        //获得session回话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //执行操作  参数：namespace+id
+        sqlSession.update("userMapper.update",user);
+
+        //mybatis执行更新操作  提交事务
+        sqlSession.commit();
+
+        //释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    //删除操作
+    public void test4() throws IOException {
+
+        //获得核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        //获得session工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        //获得session回话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //执行操作  参数：namespace+id
+        sqlSession.delete("userMapper.delete",147);
+
+        //mybatis执行更新操作  提交事务
+        sqlSession.commit();
+
+        //释放资源
+        sqlSession.close();
+    }
+
 }
